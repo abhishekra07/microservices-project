589d1a49e7c33c78f50d19fcca428978b27a1300
abhishekra07 <abhira719@gmail.com>
2023-05-16 09:45:42 +0530

    initial commit

---
+++ product-service/src/main/java/com/codelabs/productservice/model/Product.java
@@ -0,0 +1,13 @@
+package com.codelabs.productservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+}
