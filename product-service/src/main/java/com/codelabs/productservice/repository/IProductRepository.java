7ea16668b21314709c5bea9775ba721e41cef158
abhishekra07 <abhira719@gmail.com>
2023-05-17 22:00:32 +0530

    Create IProductRepository.java

---
+++ product-service/src/main/java/com/codelabs/productservice/repository/IProductRepository.java
@@ -0,0 +1,9 @@
+package com.codelabs.productservice.repository;
+
+import com.codelabs.productservice.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IProductRepository extends JpaRepository<Product, Integer> {
+}
