a76bd2ff309f2068c40fc49e78a52d544edcabec
abhishekra07 <abhira719@gmail.com>
2023-05-17 21:21:09 +0530

    Update Product.java

--- product-service/src/main/java/com/codelabs/productservice/model/Product.java
+++ product-service/src/main/java/com/codelabs/productservice/model/Product.java
@@ -1,8 +1,16 @@
 package com.codelabs.productservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Product {
     private String id;
     private String name;
