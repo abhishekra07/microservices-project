5647373ec90148d9eb535ba75edaa113ae5d214a
abhishekra07 <abhira719@gmail.com>
2023-05-17 21:42:28 +0530

    Update ProductController.java

--- product-service/src/main/java/com/codelabs/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/codelabs/productservice/controller/ProductController.java
@@ -1,8 +1,22 @@
 package com.codelabs.productservice.controller;
 
 import com.codelabs.productservice.model.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/product")
 public class ProductController {
 
+    @PostMapping
+    public void createProduct() {
 
+    }
+
+    @GetMapping
+    public void getProduct() {
+        
+    }
 }
