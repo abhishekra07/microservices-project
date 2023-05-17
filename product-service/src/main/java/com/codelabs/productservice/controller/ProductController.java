b5c239ef3ea42021eecb2ae8e97f6a7b47fc7f05
abhishekra07 <abhira719@gmail.com>
2023-05-17 22:00:19 +0530

    Update ProductController.java

--- product-service/src/main/java/com/codelabs/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/codelabs/productservice/controller/ProductController.java
@@ -2,20 +2,33 @@ package com.codelabs.productservice.controller;
 
 import com.codelabs.productservice.dto.ProductDTO;
 import com.codelabs.productservice.model.Product;
+import com.codelabs.productservice.service.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
 
-    private static final Logger logger = LoggerFactory.getLogger(Product.class);
+    private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
     @PostMapping
-    public void createProduct(@RequestBody ProductDTO productRequest) {
+    public ResponseEntity<Product> createProduct(@RequestBody ProductDTO productRequest) {
         logger.info("Received request to create new product");
-
+        Product product = productService.createProduct(productRequest);
+        logger.info("Successfully completed request to create new product :- " + product.getId());
+        return new ResponseEntity<Product>(product, HttpStatus.CREATED);
     }
 
     @GetMapping
