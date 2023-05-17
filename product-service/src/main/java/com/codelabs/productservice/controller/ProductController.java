983b18c1653d4e93c67245652df1a1fc63982a61
abhishekra07 <abhira719@gmail.com>
2023-05-17 21:47:30 +0530

    Update ProductController.java

--- product-service/src/main/java/com/codelabs/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/codelabs/productservice/controller/ProductController.java
@@ -1,17 +1,20 @@
 package com.codelabs.productservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codelabs.productservice.dto.ProductDTO;
+import com.codelabs.productservice.model.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
 
+    private static final Logger logger = LoggerFactory.getLogger(Product.class);
 
     @PostMapping
-    public void createProduct() {
+    public void createProduct(@RequestBody ProductDTO productRequest) {
+        logger.info("Received request to create new product");
 
     }
 
