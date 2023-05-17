d278421002e5db58b8e9adee81b5a0c2b01de40f
abhishekra07 <abhira719@gmail.com>
2023-05-17 21:44:49 +0530

    Create ProductService.java

---
+++ product-service/src/main/java/com/codelabs/productservice/service/ProductService.java
@@ -0,0 +1,11 @@
+package com.codelabs.productservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    public void createProduct() {
+        
+    }
+}
