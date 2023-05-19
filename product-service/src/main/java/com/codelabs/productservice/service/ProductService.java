b1448c2f029f86120e494463fc3292d83ea36f2e
abhishekra07 <abhira719@gmail.com>
2023-05-19 21:59:43 +0530

    Update ProductService.java

--- product-service/src/main/java/com/codelabs/productservice/service/ProductService.java
+++ product-service/src/main/java/com/codelabs/productservice/service/ProductService.java
@@ -1,12 +1,15 @@
 package com.codelabs.productservice.service;
 
 import com.codelabs.productservice.dto.ProductDTO;
+import com.codelabs.productservice.dto.ProductList;
 import com.codelabs.productservice.model.Product;
 import com.codelabs.productservice.repository.IProductRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -29,5 +32,18 @@ public class ProductService {
         product = repository.save(product);
         logger.info("saved product to DB");
         return product;
+    }
+
+    public ProductList getProducts() {
+        List<ProductDTO> list = repository.findAll().stream().map(product -> {
+            return ProductDTO.builder()
+                    .Id(product.getId())
+                    .name(product.getName())
+                    .description(product.getDescription())
+                    .price(product.getPrice())
+                    .build();
+        }).toList();
+        logger.info("retrieved " + list.size() + " products from database");
+        return new ProductList(list);
     }
 }
