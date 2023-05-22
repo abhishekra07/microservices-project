f0ecf9fb0912ba02d53e09c07c65f3648ecc6b84
abhishekra07 <abhira719@gmail.com>
2023-05-22 21:44:28 +0530

    Update ProductServiceApplicationTests.java

--- product-service/src/test/java/com/codelabs/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/codelabs/productservice/ProductServiceApplicationTests.java
@@ -2,12 +2,14 @@ package com.codelabs.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 class ProductServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
